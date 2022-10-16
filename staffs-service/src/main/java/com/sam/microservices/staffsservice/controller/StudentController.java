65ab5d8ae24ec6a5b1541ada2241e04bda22dfc9
achraf charif <achraf.charif@cpexterne.org>
2022-10-16 18:49:48 +0200

    start routing from the repo config server

--- staffs-service/src/main/java/com/sam/microservices/staffsservice/controller/StudentController.java
+++ staffs-service/src/main/java/com/sam/microservices/staffsservice/controller/StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController {
 
-    @GetMapping()
+    @GetMapping("student/hi")
     public String sayHi() {
         return "hi";
     }
