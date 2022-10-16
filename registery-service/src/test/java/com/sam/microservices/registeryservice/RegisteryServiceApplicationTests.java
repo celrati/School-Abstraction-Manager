10fae28656b2cdefbbe0d88b7f460787a7393fe2
achraf charif <achraf.charif@cpexterne.org>
2022-10-16 17:38:33 +0200

    registry service is done

---
+++ registery-service/src/test/java/com/sam/microservices/registeryservice/RegisteryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.microservices.registeryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisteryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
