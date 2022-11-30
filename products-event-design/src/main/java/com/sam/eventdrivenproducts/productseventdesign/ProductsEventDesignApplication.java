92dd41d61252737a5076a47b5600e44b0fd01833
achraf charif <achraf.charif@cpexterne.org>
2022-11-30 17:18:30 +0100

    first setup for the event driven service

---
+++ products-event-design/src/main/java/com/sam/eventdrivenproducts/productseventdesign/ProductsEventDesignApplication.java
@@ -0,0 +1,13 @@
+package com.sam.eventdrivenproducts.productseventdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsEventDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsEventDesignApplication.class, args);
+	}
+
+}
---
+++ products-event-design/src/test/java/com/sam/eventdrivenproducts/productseventdesign/ProductsEventDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.eventdrivenproducts.productseventdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsEventDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
